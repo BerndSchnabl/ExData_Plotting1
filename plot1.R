bf45150af58434e087ef013b22147e5a16a7f352
BerndSchnabl <bernd.schnabl@gmail.com>
2014-07-13 14:08:56 +0200

    modified:   plot1.R  	modified:   plot1.png

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-setwd("~/R/Coursera/assignments/C04-AW1")
+setwd("~/R/Coursera/assignments/C04-AW1/ExData_Plotting1")
 
 d <- read.csv("household_power_consumption.txt",sep=";", na.strings="?",header=TRUE,
               colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
@@ -8,7 +8,7 @@ d$DateTime <- strptime(paste(d$Date,d$Time),format="%d/%m/%Y %H:%M:%S")
 d$Date1 <- as.Date(d$Date, format="%d/%m/%Y")
 d1 <- d[ (d$Date == "1/2/2007") | (d$Date == "2/2/2007"),]
 
-##png(filename="plot1.png",width=480,height=480,units = "px")
+png(filename="plot1.png",width=480,height=480,units = "px")
 hist( d1$Global_active_power, 
       col = "red",
       main = "Global Actice Power",
@@ -17,4 +17,4 @@ hist( d1$Global_active_power,
 )
 
 
-##dev.off()
+dev.off()
